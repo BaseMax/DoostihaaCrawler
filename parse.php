863f10cc2c5c6a066272883c8cb6a2898a62645e
Max Base <MaxBaseCode@Gmail.Com>
2022-11-14 20:24:47 +0100

    Update parse.php

--- parse.php
+++ parse.php
@@ -122,10 +122,10 @@ function savePost(array $post) : void {
 }
 
 // Test
-$sample_link = "https://www.doostihaa.com/1401/08/19/%D9%82%D8%B3%D9%85%D8%AA-%D8%B3%DB%8C%D8%B2%D8%AF%D9%87%D9%85-%D8%B3%D8%B1%DB%8C%D8%A7%D9%84-%D8%AE%D9%88%D9%86-%D8%B3%D8%B1%D8%AF.html";
-$post = parsePost($sample_link);
+// $sample_link = "https://www.doostihaa.com/1401/08/19/%D9%82%D8%B3%D9%85%D8%AA-%D8%B3%DB%8C%D8%B2%D8%AF%D9%87%D9%85-%D8%B3%D8%B1%DB%8C%D8%A7%D9%84-%D8%AE%D9%88%D9%86-%D8%B3%D8%B1%D8%AF.html";
+// $post = parsePost($sample_link);
 // print_r($post);
-exit();
+// exit();
 
 // Main
 for ($page = 1; $page <= $total_pages; $page++) {
